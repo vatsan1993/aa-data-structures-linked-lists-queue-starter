667b9f4af439cac63e952739f4cbfcb406acd77a
Srivatsan <vatsan1993@gmail.com>
2023-10-06 16:15:32 +0530

    finished part 1 queue

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -1,27 +1,48 @@
-const { SinglyLinkedNode } = require("./01-singly-linked-list");
+const { SinglyLinkedNode } = require('./01-singly-linked-list');
 
 class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
-    constructor() {
-        this.head = null;
-        this.tail = null;
+  enqueue(val) {
+    // Add node to end of queue (linked list)
+    // Write your hypothesis on the time complexity of this method here
+    // O(1)
+    let newNode = new SinglyLinkedNode(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+      //   return this;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this.length;
+  }
+
+  dequeue() {
+    // Remove node from front of queue (linked list)
+    // Write your hypothesis on the time complexity of this method here
+    // O(1)
+    if (this.head === null) {
+      return null;
     }
 
-    enqueue(val) {
-        // Add node to end of queue (linked list)
-
-        // Write your hypothesis on the time complexity of this method here
+    let node = this.head;
+    this.head = this.head.next;
+    if (this.head === null) {
+      this.tail = null;
     }
-
-    dequeue() {
-        // Remove node from front of queue (linked list)
-        
-        // Write your hypothesis on the time complexity of this method here
-    }
-
+    this.length--;
+    return node.value;
+  }
 }
 
 module.exports = {
-    Queue,
-    SinglyLinkedNode
-}
+  Queue,
+  SinglyLinkedNode,
+};
